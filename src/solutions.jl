810790c0328c8f0b313933343d078d8be9341aee
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2022-03-25 21:23:42 -0300

    Clean up comparisons

--- src/solutions.jl
+++ src/solutions.jl
@@ -12,9 +12,9 @@ x_L(t, v₀, l) = v₀ * t
 x_R(t, v₀, l) = x_L(t, v₀, l) + l
 
 function auxF(x, v₀, l)
-    if 0 <= x && x <= (1 + v₀) * l / 2
+    if 0 <= x <= (1 + v₀) * l / 2
         -0.25 / (1.0 + v₀) * x^2
-    elseif (1 + v₀) * l / 2 < x && x <= l
+    elseif (1 + v₀) * l / 2 < x <= l
         -l^2 / 8.0 + 0.25 / (1 - v₀) * (l - x)^2
     else
         0.0
@@ -22,9 +22,9 @@ function auxF(x, v₀, l)
 end
 
 function auxf(x, v₀, l)
-    if 0 <= x && x <= (1 + v₀) * l / 2
+    if 0 <= x <= (1 + v₀) * l / 2
         -x / (1 + v₀)
-    elseif (1 + v₀) * l / 2 < x && x <= l
+    elseif (1 + v₀) * l / 2 < x <= l
         -(l - x) / (1 - v₀)
     else
         0.0
@@ -38,11 +38,11 @@ function oscillon(t, x; v₀=0.0, l=1.0)
     τₗ = τ(t, l)
     σₗ = σ(t, l)
 
-    if (x_L(τₗ, v₀, l) <= x && x <= τₗ)
+    if (x_L(τₗ, v₀, l) <= x <= τₗ)
         σₗ * (auxF(x + τₗ, v₀, l) - auxF(x - τₗ + l, v₀, l) + τₗ^2 / 2.0 - l^2 / 8.0)
-    elseif (τₗ < x && x <= l - τₗ)
+    elseif (τₗ < x <= l - τₗ)
         σₗ * (auxF(x + τₗ, v₀, l) - auxF(x - τₗ, v₀, l) + τₗ^2 / 2.0)
-    elseif (l - τₗ < x && x <= x_R(τₗ, v₀, l))
+    elseif (l - τₗ < x <= x_R(τₗ, v₀, l))
         σₗ * (auxF(x + τₗ - l, v₀, l) - auxF(x - τₗ, v₀, l) + τₗ^2 / 2.0 - l^2 / 8.0)
     else
         0.0
@@ -52,11 +52,11 @@ end
 function ∂ₜoscillon(t, x; v₀=0.0, l=1.0)
     τₗ = τ(t, l)
 
-    if x_L(τₗ, v₀, l) <= x && x <= τₗ
+    if x_L(τₗ, v₀, l) <= x <= τₗ
         0.5 * auxf(x + τₗ, v₀, l) + 0.5 * auxf(x - τₗ + l, v₀, l) + τₗ
-    elseif τₗ < x && x <= l - τₗ
+    elseif τₗ < x <= l - τₗ
         0.5 * auxf(x + τₗ, v₀, l) + 0.5 * auxf(x - τₗ, v₀, l) + τₗ
-    elseif l - τₗ < x && x <= x_R(τₗ, v₀, l)
+    elseif l - τₗ < x <= x_R(τₗ, v₀, l)
         0.5 * auxf(x + τₗ - l, v₀, l) + 0.5 * auxf(x - τₗ, v₀, l) + τₗ
     else
         0.0
@@ -67,11 +67,11 @@ function ∂ₓoscillon(t, x; v₀=0.0, l=1.0)
     τₗ = τ(t, l)
     σₗ = σ(t, l)
 
-    if x_L(τₗ, v₀, l) <= x && x <= τₗ
+    if x_L(τₗ, v₀, l) <= x <= τₗ
         σₗ * 0.5 * (auxf(x + τₗ, v₀, l) - auxf(x - τₗ + l, v₀, l))
-    elseif τₗ < x && x <= l - τₗ
+    elseif τₗ < x <= l - τₗ
         σₗ * 0.5 * (auxf(x + τₗ, v₀, l) - auxf(x - τₗ, v₀, l))
-    elseif l - τₗ < x && x <= x_R(τₗ, v₀, l)
+    elseif l - τₗ < x <= x_R(τₗ, v₀, l)
         σₗ * 0.5 * (auxf(x + τₗ - l, v₀, l) - auxf(x - τₗ, v₀, l))
     else
         0.0
