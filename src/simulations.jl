8723c15d711d067d63fcad355045b5671df0baa0
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2022-04-28 10:31:23 -0300

    Refactor simulation functions

--- src/simulations.jl
+++ src/simulations.jl
@@ -3,71 +3,74 @@ using DifferentialEquations
 export producedata
 export kink_antikink_scattering, kink_oscillon_scattering, kink_oscillon_superposition, perturbed_kink
 
-const dx = 8e-4
-
-function producedata(∂ₜφ₀, φ₀, p, tsave, save_idxs)
+function producedata(∂ₜφ₀, φ₀, (model, N, dx), tsave; sampling=10, callbacks=[])
     savedhamiltonian = SavedValues(Float64, Vector{Float64})
-    callback = SavingCallback(gethamiltonian, savedhamiltonian, saveat=tsave)
+    cbhamiltonian = SavingCallback(gethamiltonian, savedhamiltonian, saveat=tsave)
 
-    tspan = (tsave[1], tsave[end])
-    prob = SecondOrderODEProblem(fieldeq!, ∂ₜφ₀, φ₀, tspan, p)
-    sol = solve(prob, RK4(), adaptive=false, dt=1e-4, saveat=tsave, save_idxs=save_idxs, callback=callback)
+    callback = CallbackSet(cbhamiltonian, callbacks...)
+
+    tspan = (tsave[begin], tsave[end])
+    prob = SecondOrderODEProblem(fieldeq!, ∂ₜφ₀, φ₀, tspan, (model, N, dx))
+    sol = solve(prob, RK4(); adaptive=false, dt=0.125dx, saveat=tsave, save_idxs=N+1:sampling:2N, callback=callback)
 
     φ = reduce(hcat, sol.u)
     H = reduce(hcat, savedhamiltonian.saveval)
     return φ, H
 end
 
-function xgrid(tsave)
-    x = -tsave[end]:dx:tsave[end]
-    N = length(x)
-    save_idxs = N+1:10:2N
-    xsave = x[save_idxs.-N]
-
-    return x, N, save_idxs, xsave
-end
-
-function kink_antikink_scattering(V)
+function kink_antikink_scattering(V; dx=8e-4, sampling=10)
     tsave = ifelse(V < 0.5, 0.0:1e-2:20.0, 0.0:1e-2:10.0)
-    x, N, save_idxs, xsave = xgrid(tsave)
+
+    x = -tsave[end]:dx:tsave[end]
+    xsave = x[begin:sampling:end]
+    N = length(x)
 
     η₀ = kink.(0.0, -abs.(x) .+ π / γ(V), V)
     ∂ₜη₀ = ∂ₜkink.(0, -abs.(x) .+ π / γ(V), V)
 
-    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave, save_idxs)
+    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave; sampling=sampling)
     return xsave, tsave, η, H
 end
 
-function kink_oscillon_scattering(l, V, α)
+function kink_oscillon_scattering(l, V, α; dx=8e-4, sampling=10)
     tsave = ifelse(V < 0.5, 0.0:1e-2:15.0, 0.0:1e-2:10.0)
-    x, N, save_idxs, xsave = xgrid(tsave)
+
+    x = -tsave[end]:dx:tsave[end]
+    xsave = x[begin:sampling:end]
+    N = length(x)
 
     η₀ = kink.(0.0, x .+ π / γ(V), V) + oscillon.(α * l, x, l=l)
     ∂ₜη₀ = ∂ₜkink.(0.0, x .+ π / γ(V), V) + ∂ₜoscillon.(α * l, x, l=l)
 
-    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave, save_idxs)
+    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave; sampling=sampling)
     return xsave, tsave, η, H
 end
 
-function kink_oscillon_superposition(l, α)
+function kink_oscillon_superposition(l, α; dx=8e-4, sampling=10)
     tsave = 0.0:1e-2:10.0
-    x, N, save_idxs, xsave = xgrid(tsave)
+
+    x = -tsave[end]:dx:tsave[end]
+    xsave = x[begin:sampling:end]
+    N = length(x)
 
     η₀ = kink.(x .+ π / 2) + oscillon.(α * l, x .+ l / 2, l=l)
     ∂ₜη₀ = ∂ₜoscillon.(α * l, x .+ l / 2, l=l)
 
-    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave, save_idxs)
+    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave; sampling=sampling)
     return xsave, tsave, η, H
 end
 
 
-function perturbed_kink(ϵ)
+function perturbed_kink(ϵ; dx=8e-4, sampling=10)
     tsave = 0.0:1e-2:10.0
-    x, N, save_idxs, xsave = xgrid(tsave)
+
+    x = -tsave[end]:dx:tsave[end]
+    xsave = x[begin:sampling:end]
+    N = length(x)
 
     η₀ = kink.(x / (1.0 + ϵ) .+ π / 2)
     ∂ₜη₀ = zero(x)
 
-    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave, save_idxs)
+    η, H = producedata(∂ₜη₀, η₀, (quadratic, N, dx), tsave; sampling=sampling)
     return xsave, tsave, η, H
 end
