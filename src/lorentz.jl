c827dbf3103fee3fadf5ef06ea36d739362b62ee
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2022-05-09 12:01:59 -0300

    Add documentation for Lorentz transformations

--- src/lorentz.jl
+++ src/lorentz.jl
@@ -1,7 +1,17 @@
 export γ, boost
 
+"""
+    γ(V)
+
+Lorentz factor ``1 / √(1-V²)``.
+"""
 γ(V) = 1.0 / √(1 - V^2)
 
+"""
+    boost(t, x, V)
+
+Return coordinates ``(t′, x′)`` in frame of reference moving uniformly with velocity ``V``.
+"""
 function boost(t, x, V)
     t′ = γ(V) * (t - V * x)
     x′ = γ(V) * (x - V * t)
