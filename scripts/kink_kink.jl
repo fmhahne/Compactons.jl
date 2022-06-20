9d17b1f1a48048a513d3561f45d2af4997ae0ac0
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2022-06-20 19:31:34 -0300

    Generate kink-kink scattering plot

--- scripts/kink_kink.jl
+++ scripts/kink_kink.jl
@@ -3,11 +3,12 @@ using Compactons
 include(srcdir("plots.jl"))
 
 V = 0.65
-data, _ = produce_or_load(datadir("kink_kink"), KinkKink(V), simulation)
-@unpack x, t, η, H = data
+params = KinkKink(V)
+data, _ = produce_or_load(datadir("kink_kink"), params, simulation)
+@unpack x, t, η = data
 
-fig, axs = plt.subplots(1, 2; sharey=true)
-heatmap!(axs[1], x, t, η; colorbar=true, norm=mpl.colors.CenteredNorm())
-heatmap!(axs[2], x, t, H; colorbar=true, cmap="magma", norm=mpl.colors.SymLogNorm(1e-5))
-axs[2].set_ylabel(nothing)
+fig, ax = plt.subplots()
+heatmap!(ax, x, t, η; colorbar=true, norm=mpl.colors.CenteredNorm())
+ax.set_xlim(-10V, 10V)
+fig.savefig(plotsdir("kink_kink", savename(params, "pdf")))
 fig
