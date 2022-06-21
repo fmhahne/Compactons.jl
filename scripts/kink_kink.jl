704253ac9826a49874096d7ac12c2a5c82451a96
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2022-06-20 21:13:17 -0300

    Kink-kink scattering: plot hamiltonian

--- scripts/kink_kink.jl
+++ scripts/kink_kink.jl
@@ -5,10 +5,17 @@ include(srcdir("plots.jl"))
 V = 0.65
 params = KinkKink(V)
 data, _ = produce_or_load(datadir("kink_kink"), params, simulation)
-@unpack x, t, η = data
+@unpack x, t, η, H = data
 
-fig, ax = plt.subplots()
-heatmap!(ax, x, t, η; colorbar=true, norm=mpl.colors.CenteredNorm())
-ax.set_xlim(-10V, 10V)
-fig.savefig(plotsdir("kink_kink", savename(params, "pdf")))
+fig, axs = plt.subplots(1, 2; figsize=(6.2, 2.5), sharey=true)
+
+heatmap!(axs[1], x, t, η; colorbar=true, norm=mpl.colors.CenteredNorm())
+axs[1].set_xlim(-10V, 10V)
+axs[1].set_title(raw"$\eta(t,x)$")
+
+heatmap!(axs[2], x, t, H; colorbar=true, cmap="magma", norm=mpl.colors.SymLogNorm(1e-5))
+axs[2].set_xlim(-10V, 10V)
+axs[2].set_title(raw"$\mathcal{H}(t,x)$")
+
+fig.savefig(plotsdir("kink_kink", "example.pdf"))
 fig
