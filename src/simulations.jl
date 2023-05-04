4ff88deb8d40bc08ed480961a38376e0d7dee4f2
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2023-05-04 20:36:48 -0300

    Drop unrelated kink scattering simulations

--- src/simulations.jl
+++ src/simulations.jl
@@ -36,50 +36,6 @@ function simulation(parameters::NonBPSKink; dx=1e-3, sampling=10)
     return Dict("x" => xsave, "t" => tsave, "η" => η, "H" => H)
 end
 
-@with_kw struct KinkAntikink{T<:Real}
-    V::T
-    dx::T = 1e-3
-    dt::T = 0.1dx
-    tmax::T = 10.0
-    sampling::Int = 10
-end
-
-function simulation(parameters::KinkAntikink)
-    @unpack V, dx, dt, tmax, sampling = parameters
-    tsave = 0.0:(dx*sampling):tmax
-
-    x = -tmax:dx:tmax
-    xsave = x[begin:sampling:end]
-
-    η₀ = kink.(0.0, -abs.(x) .+ π / γ(V), V)
-    ∂ₜη₀ = ∂ₜkink.(0, -abs.(x) .+ π / γ(V), V)
-
-    η, H = producedata(quadratic, ∂ₜη₀, η₀, tsave; dx, dt, sampling)
-    return Dict("x" => xsave, "t" => tsave, "η" => η, "H" => H)
-end
-
-@with_kw struct KinkKink{T<:Real}
-    V::T
-    dx::T = 1e-3
-    dt::T = 0.1dx
-    tmax::T = 10.0
-    sampling::Int = 10
-end
-
-function simulation(parameters::KinkKink; dx=1e-3, sampling=10)
-    @unpack V, dx, dt, tmax, sampling = parameters
-    tsave = 0.0:(dx*sampling):tmax
-
-    x = -tmax:dx:tmax
-    xsave = x[begin:sampling:end]
-
-    η₀ = @. kink(0.0, x + π / γ(V), V) + kink(0.0, x, -V) - 2
-    ∂ₜη₀ = @. ∂ₜkink(0, x + π / γ(V), V) + ∂ₜkink(0.0, x, -V)
-
-    η, H = producedata(quadratic, ∂ₜη₀, η₀, tsave; dx, sampling=sampling, dt=0.1dx)
-    return Dict("x" => xsave, "t" => tsave, "η" => η, "H" => H)
-end
-
 @with_kw struct KinkOscillon{T<:Real}
     l::T
     V::T
