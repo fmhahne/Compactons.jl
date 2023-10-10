367240c19bd6174b5af9d1ad51162eff1c0bc96c
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2023-10-09 23:45:37 -0300

    Enforce style in produce_data function

--- src/simulations.jl
+++ src/simulations.jl
@@ -1,8 +1,8 @@
 function produce_data(model, ∂ₜϕ₀, ϕ₀, tsave; dx, dt=dx / 10, sampling=10, callbacks=[])
-    savedhamiltonian = SavedValues(Float64, Vector{Float64})
-    cbhamiltonian = SavingCallback(get_hamiltonian, savedhamiltonian; saveat=tsave)
+    saved_hamiltonian = SavedValues(Float64, Vector{Float64})
+    cb_hamiltonian = SavingCallback(get_hamiltonian, saved_hamiltonian; saveat=tsave)
 
-    callback = CallbackSet(cbhamiltonian, callbacks...)
+    callback = CallbackSet(cb_hamiltonian, callbacks...)
 
     N = length(ϕ₀)
     tspan = (tsave[begin], tsave[end])
@@ -12,14 +12,14 @@ function produce_data(model, ∂ₜϕ₀, ϕ₀, tsave; dx, dt=dx / 10, sampling
         prob,
         RK4();
         adaptive=false,
-        dt=dt,
+        callback,
+        dt,
         saveat=tsave,
         save_idxs=(N + 1):sampling:(2N),
-        callback=callback,
     )
 
     ϕ = reduce(hcat, sol.u)
-    H = reduce(hcat, savedhamiltonian.saveval)
+    H = reduce(hcat, saved_hamiltonian.saveval)
 
     return ϕ, H
 end
