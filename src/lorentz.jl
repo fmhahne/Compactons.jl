8dffc90bdf4afa1d13f94ab14f954ef256b8f2f2
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2023-10-09 23:46:45 -0300

    Use lowercase v for Lorentz transforms

--- src/lorentz.jl
+++ src/lorentz.jl
@@ -1,18 +1,18 @@
 """
-    γ(V)
+    γ(v)
 
-Lorentz factor ``1 / √(1-V²)``.
+Lorentz factor ``1 / √(1-v²)``.
 """
-γ(V) = 1.0 / √(1 - V^2)
+γ(v) = 1.0 / √(1 - v^2)
 
 """
-    boost(t, x, V)
+    boost(t, x, v)
 
-Return coordinates ``(t′, x′)`` in frame of reference moving uniformly with velocity ``V``.
+Return coordinates ``(t′, x′)`` in frame of reference moving uniformly with velocity ``v``.
 """
-function boost(t, x, V)
-    t′ = γ(V) * (t - V * x)
-    x′ = γ(V) * (x - V * t)
+function boost(t, x, v)
+    t′ = γ(v) * (t - v * x)
+    x′ = γ(v) * (x - v * t)
 
     return (t′, x′)
 end
