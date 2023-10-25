d0148daa86cf6bff1e74f519bd5867b87d65ba9d
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2023-10-24 23:54:43 -0300

    Add large velocities to closest approximation plot

--- scripts/kink_kink/closest_approximation.jl
+++ scripts/kink_kink/closest_approximation.jl
@@ -3,24 +3,15 @@ using Compactons
 include(srcdir("plots.jl"))
 
 tmax = 20.0
-vsave = 0:1e-4:0.01
+vsave1 = 0.0:0.002:0.70
+vsave2 = 0.0:0.002:0.87
 xRmins = Float64[]
 xRmins_non_rel = Float64[]
 xRmins_non_rel_mode = Float64[]
 xRmins_rel = Float64[]
 xRmins_rel_mode = Float64[]
 
-for v in vsave
-    params = KinkKinkBorder(; v, xmax=5.0, tmax)
-    data, _ = produce_or_load(datadir("kink_kink_border"), params, simulation)
-    @unpack x, xR_idxs = data
-
-    xRs = Float64[]
-    for i in xR_idxs.saveval
-        append!(xRs, x[i])
-    end
-    append!(xRmins, minimum(xRs))
-
+for v in vsave1
     params = CCKinkKinkNonRel(; v, tmax)
     data, _ = produce_or_load(
         datadir("cc_kink_kink_non_rel"), params, collective_coordinates
@@ -34,6 +25,17 @@ for v in vsave
     )
     @unpack a = data
     append!(xRmins_non_rel_mode, π / 2 + minimum(a))
+end
+
+for v in vsave2
+    params = KinkKinkBorder(; v, xmax=tmax, tmax)
+    data, _ = produce_or_load(datadir("kink_kink_border"), params, simulation)
+    @unpack x, xR_idxs = data
+    xRs = Float64[]
+    for i in xR_idxs.saveval
+        append!(xRs, x[i])
+    end
+    append!(xRmins, minimum(xRs))
 
     params = CCKinkKinkRel(; v, tmax)
     data, _ = produce_or_load(datadir("cc_kink_kink_rel"), params, collective_coordinates)
@@ -48,14 +50,12 @@ for v in vsave
     append!(xRmins_rel_mode, minimum(@. π / (2 * b) + a))
 end
 
-fig, ax = plt.subplots()
-ax.scatter(vsave, xRmins; color="black", marker="o", s=3, label="Simulation")
-ax.scatter(vsave, xRmins_non_rel; marker="x", s=3, label="Non-relativistic")
-ax.scatter(
-    vsave, xRmins_non_rel_mode; marker="+", s=3, label="Non-relativistic with internal mode"
-)
-ax.scatter(vsave, xRmins_rel; marker="D", s=3, label="Relativistic")
-ax.scatter(vsave, xRmins_rel_mode; marker="^", s=3, label="Relativistic with internal mode")
+fig, ax = plt.subplots(; figsize=(4.5, 3.0))
+ax.plot(vsave2, xRmins; label="Simulation")
+ax.plot(vsave1, xRmins_non_rel; label="Non-relativistic")
+ax.plot(vsave1, xRmins_non_rel_mode; label="Non-relativistic with internal mode")
+ax.plot(vsave2, xRmins_rel; label="Relativistic")
+ax.plot(vsave2, xRmins_rel_mode; label="Relativistic with internal mode")
 ax.set_xlabel(raw"$v$")
 ax.set_ylabel(raw"$x_{R,\mathrm{min}}$")
 ax.legend(; frameon=true)
