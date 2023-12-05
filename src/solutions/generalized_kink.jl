edcd3840e9f1d73e2f762b7ce94765c8eac028b6
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2023-12-05 15:48:03 -0300

    Fix generalized kink

--- src/solutions/generalized_kink.jl
+++ src/solutions/generalized_kink.jl
@@ -11,11 +11,11 @@ end
 Field ``η(t, x)`` for a kink at rest in the generalized model with parameter ``k``.
 """
 function generalized_kink(t, x; k)
-    if x < 0
-        0
-    elseif x ≤ 2 * x₀(k)
-        fzero(η -> bps(η, x, k), (0, 2))
+    if x <= 0
+        0.0
+    elseif x < 2 * x₀(k)
+        find_zero(η -> bps(η, x, k), (0.0, 2.0))
     else
-        2
+        2.0
     end
 end
